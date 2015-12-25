ca9b59c47737c0adb868b806ec641e92bb415738
Jason Kuhrt <jasonkuhrt@me.com>
2015-12-24 22:54:37 -0500

    Setup Main

--- chapter-13/hangman/source/Main.hs
+++ chapter-13/hangman/source/Main.hs
@@ -1,1 +1,12 @@
 module Main where
+
+import Control.Monad (forever)
+import Data.Char (toLower)
+import Data.Maybe (isJust)
+import System.Exit (exitSuccess)
+import System.Random (randomRIO)
+
+
+
+main :: IO ()
+main = undefined
