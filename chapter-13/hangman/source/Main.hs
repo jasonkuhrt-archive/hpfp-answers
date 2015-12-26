3a0d6e5bd64a0eef5298496fde800d2f90f7f13e
Jason Kuhrt <jasonkuhrt@me.com>
2015-12-25 21:15:40 -0500

    Functions for reading a word list from the file system and picking random words from it. This also introduces a few game rules related to minimum and maximum allowed word lengths.

--- chapter-13/hangman/source/Main.hs
+++ chapter-13/hangman/source/Main.hs
@@ -8,5 +8,57 @@ import System.Random (randomRIO)
 
 
 
+-- Type `Word` is already taken so we use the terminology "werd" in place
+-- of "word" throughout this program.
+
+type Werd = String
+type WerdList = [Werd]
+
+
+
 main :: IO ()
 main = undefined
+
+
+
+-- Game
+
+randomWerd :: IO Werd
+randomWerd = gameWerdList >>= randomWerdFromList
+
+gameWerdList :: IO WerdList
+gameWerdList = do
+  werds <- readWerdList
+  return (filter fitsGame werds)
+  where
+    fitsGame :: Werd -> Bool
+    fitsGame werd = let l = length werd
+                     in l >= gameMinWerdLength &&
+                        l <= gameMaxWerdLength
+
+gameMinWerdLength :: Int
+gameMinWerdLength = 5
+
+gameMaxWerdLength :: Int
+gameMaxWerdLength = 10
+
+
+
+-- Working with Word lists
+
+randomWerdFromList :: WerdList -> IO Werd
+randomWerdFromList werdList = do
+  i <- randomItem werdList
+  return (werdList !! i)
+
+readWerdList :: IO WerdList
+readWerdList = do
+  dict <- readFile "data/dict.txt"
+  return (lines dict)
+
+
+
+-- Utilities
+
+randomItem :: [a] -> IO Int
+randomItem xs = randomRIO (0, length xs - 1)
