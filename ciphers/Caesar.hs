672ac685a791ffe4b9652f11a51cfffa0910e867
Jason Kuhrt <jasonkuhrt@me.com>
2016-01-01 21:07:24 -0500

    Fix non-exhaustive pattern match bug in nested go function. Missing termination case for recusrive encryption where given string reaches empty list case.

--- ciphers/Caesar.hs
+++ ciphers/Caesar.hs
@@ -8,17 +8,20 @@ module Caesar where
 -- that as long as the Key is only known to trusted parties strangers
 -- will not be able to read the StringEncrypted text.
 
+import Data.Char
+
 
 
 type Key = String
 type StringEncrypted = String
 
 caesarCipher :: Key -> String -> StringEncrypted
-caesarCipher _ "" = ""
-caesarCipher key string = go (cycle key) string
-  where
-  go ks (' ':xs)   = ' ' : go ks xs
-  go (k:ks) (x:xs) = encryptChar k x : go ks xs
+caesarCipher key = caesarCipherDo (cycle key)
+
+caesarCipherDo :: Key -> String -> StringEncrypted
+caesarCipherDo _ ""          = ""
+caesarCipherDo ks (' ':xs)   = ' ' : caesarCipherDo ks xs
+caesarCipherDo (k:ks) (x:xs) = encryptChar k x : caesarCipherDo ks xs
 
 encryptChar :: Char -> Char -> Char
 encryptChar keyChar char = chars !! index
