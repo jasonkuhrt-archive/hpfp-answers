e9172e3c6082b06f537f860fc6d82184c5ad0e4e
Jason Kuhrt <jasonkuhrt@me.com>
2016-01-07 20:35:53 -0500

    Fix spelling

--- chapter-13/Palindrome.hs
+++ chapter-13/Palindrome.hs
@@ -1,4 +1,4 @@
-module Palindrom where
+module Palindrome where
 
 import System.Exit (exitSuccess)
 import Control.Monad (forever)
@@ -7,12 +7,12 @@ import Control.Monad (forever)
 
 main :: IO ()
 main = forever $ do
-  putStrLn "Please enter a palindrom:"
+  putStrLn "Please enter a Palindrome:"
   word <- getLine
   if word == reverse word
     then do
-      putStrLn "Yup, that's a palindrom! Bye."
+      putStrLn "Yup, that's a Palindrome! Bye."
       exitSuccess
     else do
-      putStrLn $ "Whoops, \"" ++ word ++ "\" is invalid. Try again. A palindrom must have the same order of letters in either direction (e.g. mom)."
+      putStrLn $ "Whoops, \"" ++ word ++ "\" is invalid. Try again. A Palindrome must have the same order of letters in either direction (e.g. mom)."
       main
