7190a8bd2eebbb4763cb4bfcd39549d4d7b170f9
Jason Kuhrt <jasonkuhrt@me.com>
2016-01-07 20:58:11 -0500

    Make Palindrome work on phrases or words. For example "Madam I’m Adam," will work now.

--- chapter-13/Palindrome.hs
+++ chapter-13/Palindrome.hs
@@ -1,5 +1,6 @@
 module Palindrome where
 
+import Data.Char (toLower)
 import System.Exit (exitSuccess)
 import Control.Monad (forever)
 
@@ -9,10 +10,22 @@ main :: IO ()
 main = forever $ do
   putStrLn "Please enter a Palindrome:"
   word <- getLine
-  if word == reverse word
+  if isPalindrome word
     then do
       putStrLn "Yup, that's a Palindrome! Bye."
       exitSuccess
     else do
       putStrLn $ "Whoops, \"" ++ word ++ "\" is invalid. Try again. A Palindrome must have the same order of letters in either direction (e.g. mom)."
       main
+
+
+meaingfulChars :: String
+meaingfulChars = ['A'..'Z'] ++ ['a'..'z'] ++ ['0'..'9']
+
+isPalindrome :: String -> Bool
+isPalindrome string = stringMeaningful == reverse stringMeaningful
+  where
+  stringMeaningful =
+    map toLower .
+    filter (`elem` meaingfulChars) $
+    string
