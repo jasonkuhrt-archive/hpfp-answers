d639f8802eddfb3ae0c295a7f838691cc24f4e0f
Jason Kuhrt <jasonkuhrt@me.com>
2016-01-23 09:20:20 -0500

    Test a new property. The nature of sorted lists.

--- chapter-14/QuickCheckTests/QuickCheckTests.hs
+++ chapter-14/QuickCheckTests/QuickCheckTests.hs
@@ -1,26 +1,47 @@
 module QuickCheckTests where
 
 import Test.QuickCheck
+import Data.List
 
 
 
 main :: IO ()
 main = do
-  quickCheck halfDoubledIdentity
-  quickCheck twoHalvesIdentity
+  quickCheck halfAddHalfIdentProp
+  quickCheck halfAddHalfIdentityProp
+  quickCheck orderedListProp
 
 
 
+-- 1 Half
 
-halfDoubledIdentity = forAll generator test
+halfAddHalfIdentProp = forAll generator test
   where
   generator = arbitrary :: Gen Float
   test n = n == (half n * 2)
 
-twoHalvesIdentity = forAll generator test
+halfAddHalfIdentityProp = forAll generator test
   where
   generator = arbitrary :: Gen Float
   test n = n == (half n + half n)
 
 half :: Float -> Float
 half n = n / 2
+
+
+
+-- 2
+
+-- TODO Manually write orderedList
+-- TODO Type-level generator of any type in the Ord typeclass
+orderedListProp = forAll generator test
+  where
+  generator = orderedList :: Gen [Float]
+  test = isListOrdered
+
+isListOrdered :: Ord a => [a] -> Bool
+isListOrdered xs = snd $ foldr go (Nothing, True) xs
+  where
+  go _ status@(_, False)      = status
+  go x (Just xPrevious, flag) = (Just x, xPrevious >= x)
+  go x (Nothing, flag)        = (Just x, flag) -- first iteration
