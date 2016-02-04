ce8bf38b1558d16ca60d34f73f5fa71b6670d55e
Jason Kuhrt <jasonkuhrt@me.com>
2016-02-04 14:57:44 -0500

    [CH15] First exercise

---
+++ chapter-15-monoid-semigroup/Optional.hs
@@ -0,0 +1,29 @@
+module Optional where
+
+import Data.Monoid
+
+
+
+data Optional a =
+    Nada
+  | Only a
+  deriving (Show, Eq)
+
+instance Monoid a => Monoid (Optional a) where
+  mempty = Nada
+  mappend Nada x = x
+  mappend x Nada = x
+  mappend (Only x) (Only z) = Only (mappend x z)
+
+
+
+-- Tests
+
+t1 :: Num a => Optional (Sum a)
+t1 = Only (Sum 1) <> Only (Sum 1)
+
+t2 :: Num a => Optional (Sum a)
+t2 = Only (Sum 1) <> Nada
+
+t3 :: Num a => Optional (Sum a)
+t3 = Nada <> Only (Sum 1)
