7d228bbc427ab691cf08150036128f2a815591a7
Jason Kuhrt <jasonkuhrt@me.com>
2016-02-07 13:12:21 -0500

    Finish Semigroup exercises

--- chapter-15-monoid-semigroup/Semigroup/Instances.hs
+++ chapter-15-monoid-semigroup/Semigroup/Instances.hs
@@ -16,6 +16,9 @@ main = do
   quickCheck (isSemigroupAssoc :: BoolConj -> BoolConj -> BoolConj -> Bool)
   quickCheck (isSemigroupAssoc :: BoolDisj -> BoolDisj -> BoolDisj -> Bool)
   quickCheck (isSemigroupAssoc :: Or String String -> Or String String -> Or String String -> Bool)
+  quickCheck (isSemigroupAssoc :: Validation String String -> Validation String String -> Validation String String -> Bool)
+  quickCheck (isSemigroupAssoc :: AR String String -> AR String String -> AR String String -> Bool)
+  quickCheck (isSemigroupAssoc :: AB String String -> AB String String -> AB String String -> Bool)
 
 
 
@@ -146,7 +149,7 @@ instance (Arbitrary a, Arbitrary b) => Arbitrary (Or a b) where
 newtype Combine a b = Combine { unCombine :: a -> b }
 
 instance (Semigroup b) => Semigroup (Combine a b) where
-  (Combine f1) <> (Combine f2) = Combine (\x -> (f1 x) <> (f2 x))
+  (Combine f1) <> (Combine f2) = Combine (\x -> f1 x <> f2 x)
 
 -- Testing this requires understanding CoArbitrary which I do not understand yet. So, the following is a manual test we can use:
 
@@ -156,3 +159,79 @@ g = Combine (\n -> Sum (n - 1))
 -- 2 == getSum ((unCombine (f <> g)) 1)
 -- 2 == getSum ((unCombine (g <> f)) 1)
 -- 4 == getSum ((unCombine (f <> f)) 1)
+
+
+
+-- 10
+
+newtype Comp a =  Comp { unComp :: a -> a }
+
+instance Semigroup (Comp a) where
+  (Comp f1) <> (Comp f2) = Comp (f2 . f1)
+
+f2 = Combine (\n -> Sum (n + 1))
+g2 = Combine (\n -> Sum (n - 1))
+-- 0 == getSum ((unCombine (f2 <> g2)) 0)
+-- 2 == getSum ((unCombine (f2 <> g2)) 1)
+-- 2 == getSum ((unCombine (g2 <> f2)) 1)
+-- 4 == getSum ((unCombine (f2 <> f2)) 1)
+
+
+
+-- 11
+
+data Validation a b =
+    Failed a
+  | OK b
+  deriving (Eq, Show)
+
+instance Semigroup a => Semigroup (Validation a b) where
+  x@(OK _) <> _             = x
+  _ <> x@(OK _)             = x
+  (Failed x) <> (Failed z)  = Failed (x <> z)
+
+instance (Arbitrary a, Arbitrary b) => Arbitrary (Validation a b) where
+  arbitrary = do
+    a <- arbitrary
+    b <- arbitrary
+    elements [Failed a, OK b]
+
+
+
+-- 12
+
+-- AR stands for AccumulateRight
+newtype AR a b = AR (Validation a b)
+  deriving (Eq, Show)
+
+instance (Semigroup b) => Semigroup (AR a b) where
+  (AR (OK x)) <> (AR (OK z))  = AR (OK (x <> z))
+  x@(AR (OK _)) <> _          = x
+  _ <> x@(AR (OK _))          = x
+  x <> _                      = x
+
+instance (Arbitrary a, Arbitrary b) => Arbitrary (AR a b) where
+  arbitrary = do
+    a <- arbitrary
+    b <- arbitrary
+    elements [AR (Failed a), AR (OK b)]
+
+
+
+-- 13
+
+-- AB stands for AccumulateBoth
+newtype AB a b = AB (Validation a b)
+  deriving (Eq, Show)
+
+instance (Semigroup a, Semigroup b) => Semigroup (AB a b) where
+  (AB (OK x)) <> (AB (OK z))          = AB (OK (x <> z))
+  x@(AB (OK _)) <> _                  = x
+  _ <> x@(AB (OK _))                  = x
+  (AB (Failed x)) <> (AB (Failed z))  = AB (Failed (x <> z))
+
+instance (Arbitrary a, Arbitrary b) => Arbitrary (AB a b) where
+  arbitrary = do
+    a <- arbitrary
+    b <- arbitrary
+    elements [AB (Failed a), AB (OK b)]
