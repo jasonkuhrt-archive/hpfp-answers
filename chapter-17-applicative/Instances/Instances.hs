ce046f1c84e7b89f265c91f908ba34f3b4c8ddb1
Jason Kuhrt <jasonkuhrt@me.com>
2016-02-18 10:09:19 -0500

    [CH-17] Applicative instance for Three data

--- chapter-17-applicative/Instances/Instances.hs
+++ chapter-17-applicative/Instances/Instances.hs
@@ -16,8 +16,13 @@ test = do
   quickBatch $ functor $ (undefined :: Pair (S,S,S))
   quickBatch $ applicative $ (undefined :: Pair (S,S,S))
 
+  quickBatch $ monoid $ (undefined :: Two S (S,S,S))
   quickBatch $ functor $ (undefined :: Two S (S,S,S))
   quickBatch $ applicative $ (undefined :: Two S (S,S,S))
+
+  quickBatch $ monoid $ (undefined :: Three S S (S,S,S))
+  quickBatch $ functor $ (undefined :: Three S S (S,S,S))
+  quickBatch $ applicative $ (undefined :: Three S S (S,S,S))
 
 
 
@@ -82,4 +87,30 @@ instance (Arbitrary a, Arbitrary b) => Arbitrary (Two a b) where
     pure $ Two x z
 
 instance (Eq a, Eq b) => EqProp (Two a b) where
+  (=-=) = eq
+
+
+
+data Three a b c = Three a b c
+  deriving (Eq, Show)
+
+instance Functor (Three a b) where
+  fmap f (Three u x z) = Three u x (f z)
+
+instance (Monoid a, Monoid b, Monoid c) => Monoid (Three a b c) where
+  mempty = Three mempty mempty mempty
+  mappend (Three v x z) (Three v' x' z') = Three (mappend v v') (mappend x x') (mappend z z')
+
+instance (Monoid a, Monoid b) => Applicative (Three a b) where
+  pure x = Three mempty mempty x
+  (<*>) (Three v x f) (Three v' x' z) = Three (mappend v v') (mappend x x') (f z)
+
+instance (Arbitrary a, Arbitrary b, Arbitrary c) => Arbitrary (Three a b c) where
+  arbitrary = do
+    v <- arbitrary
+    x <- arbitrary
+    z <- arbitrary
+    pure $ Three v x z
+
+instance (Eq a, Eq b, Eq c) => EqProp (Three a b c) where
   (=-=) = eq
