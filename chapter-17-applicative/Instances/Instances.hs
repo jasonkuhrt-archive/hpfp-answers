08b97bb7008f069a01c61b9d072b2d3ba0dd96d5
Jason Kuhrt <jasonkuhrt@me.com>
2016-02-18 09:46:31 -0500

    [CH-17] Pair instance for Identity data

--- chapter-17-applicative/Instances/Instances.hs
+++ chapter-17-applicative/Instances/Instances.hs
@@ -5,10 +5,14 @@ import Test.QuickCheck.Classes
 import Test.QuickCheck.Checkers
 
 
+type S = String
 test :: IO ()
 test = do
   quickBatch $ functor $ Identity ("","","")
   quickBatch $ applicative $ Identity ("","","")
+
+  quickBatch $ functor $ (undefined :: Pair (S,S,S))
+  quickBatch $ applicative $ (undefined :: Pair (S,S,S))
 
 
 
@@ -26,4 +30,25 @@ instance (Arbitrary a) => Arbitrary (Identity a) where
   arbitrary = pure Identity <*> arbitrary
 
 instance (Eq a) => EqProp (Identity a) where
+  (=-=) = eq
+
+
+
+data Pair a = Pair a a
+  deriving (Eq, Show)
+
+instance Functor Pair where
+  fmap f (Pair x z) = Pair (f x) (f z)
+
+instance Applicative Pair where
+  pure x = Pair x x
+  (<*>) (Pair f1 f2) (Pair x z) = Pair (f1 x) (f2 z)
+
+instance (Arbitrary a) => Arbitrary (Pair a) where
+  arbitrary = do
+    x <- arbitrary
+    z <- arbitrary
+    pure $ Pair x z
+
+instance (Eq a) => EqProp (Pair a) where
   (=-=) = eq
